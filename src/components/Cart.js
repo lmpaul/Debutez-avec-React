603b976a3e2b11da42cf822eefed0fcdb5acda3a
Paul Le Mercier <paul.lemercier05@gmail.com>
2022-10-28 10:30:12 +0400

    m

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -18,7 +18,11 @@ function Cart() {
 			<div>Monstera : {monsteraPrice}€</div>
 			<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
 			<h3>Total : {monsteraPrice * cart}€</h3>
+      <button onClick={() => updateCart(0)}>
+        Vider le panier
+      </button>
 		</div>
+
 	) : (
 		<div className='lmj-cart-closed'>
 			<button
